f3bec17eaba9b76a7ce1103d89d3de14f586ff3e
Mausam Gerra <maugerra@iu.edu>
2026-01-05 00:58:48 -0500

    Added vercel url to CORS

--- fitness_marketplace_backend/app/main.py
+++ fitness_marketplace_backend/app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://fitness-marketplace-frontend.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
